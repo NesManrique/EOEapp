4c150fddde4987b28a4b8a3e898e1b707ba98ff1
Jon Ander <snoopy@droopy.(none)>
2012-12-04 21:15:26 -0430

    Resolvi un bug en sesiones

--- project/Build.scala
+++ project/Build.scala
@@ -4,11 +4,7 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-<<<<<<< HEAD
-    val appName         = "playapp"
-=======
-    val appName         = "EOEapp"
->>>>>>> 87e52c0f3ace41d80b09dcbf339d70bfbd9de347
+    val appName         = "prueba"
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
